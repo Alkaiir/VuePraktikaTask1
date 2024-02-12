2fa9c992e39c810eb9d45e9d6db950b147756ff7
Иван Венцов <gr412_veini@lab-407-10.tomtit-tomsk.ru>
2024-02-12 11:43:45 +0700

    7 Практическая работа

--- js/main.js
+++ js/main.js
@@ -2,30 +2,54 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
+        onSale: true,
         altText: "A pair of socks",
-        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0
             }
         ],
+
         cart: 0
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.cart += 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
+        }
+
+    },
+    computed: {
+
+        inStock() {
+          return this.variants[this.selectedVariant].variantQuantity;
+        },
+
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+
+        sale() {
+            return this.brand + ' ' + this.product + 's currently holding a sale';
         }
     }
 })
